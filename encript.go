e0c4a90c12733e41335fda8ca222f4b1f77ac4b7
JasonBoyett <jason.boyett@gmail.com>
2023-08-23 18:05:23 -0500

    "first itteration of encryption"

--- encript.go
+++ encript.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"crypto/sha256"
+)
+
 func Encript(token1 string, token2 string) (string, error) {
-  return "Hello, World!", nil
+  combined := token1 + token2
+  if len(combined) == 0 {
+    return Encript("Allen", "Turing")
+  }
+
+  hash := sha256.Sum256([]byte(combined))
+  return string(hash[:]), nil
 }
+
